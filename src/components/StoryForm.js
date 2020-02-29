97068938c9a1ae3a34fcd3d6ba6ad148a0e8566d
Joscelyn <JoscelynRose@gmail.com>
2020-02-29 14:13:14 -0500

    made progress on adding material ui to the form

--- src/components/StoryForm.js
+++ src/components/StoryForm.js
@@ -1,99 +1,145 @@
 import React from 'react';
-import axios from 'axios';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from 'yup';
+import Avatar from '@material-ui/core/Avatar';
 import NavBar from './NavBar.js';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import Footer from './Footer.js';
 
-const StoryForm = () => {
+function Copyright() {
   return (
-    <div className="form-container">
-      <NavBar />
-      <div className="form-text">
-        <h1 className="h1">Share Your Story</h1>
-        <p className="p story-text">
-          Please share your story with us. After our admins have reviewed it, we
-          will do our part to share it with the world.
-        </p>
-      </div>
-
-      <Form className="signup-form">
-        <div className="main-fields">
-          <div className="field-container">
-            <label>Author</label>
-            <Field className="form-field" name="author" type="text" />
-          </div>
-          <div className="field-container">
-            <label>Country of Origin</label>
-            <Field className="form-field" name="country" type="text" />
-          </div>
-          <div className="field-container">
-            <label>Title</label>
-            <Field className="form-field" name="title" type="text" />
-          </div>
-          <div className="field-container">
-            <label>Year the Story Took Place</label>
-            <Field className="form-field" name="year" type="number" />
-          </div>
-          <div className="field-container">
-            <label>Image</label>
-            <Field className="form-field" name="imageUrl" type="text" />
-          </div>
-        </div>
-        <div>
-          <div className="field-container">
-            <label>Your Story</label>
-            <Field
-              className="story-form-field"
-              component="textarea"
-              name="story"
-              type="text"
-            />
-            <button type="button" className="submit-button btn">
-              Submit
-            </button>
-          </div>
-        </div>
-      </Form>
-      <Footer />
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
-};
+}
 
-const FormikStoryForm = withFormik({
-  mapPropsToValues({ author, country, title, story, year, imageUrl }) {
-    return {
-      author: author || '',
-      country: country || '',
-      title: title || '',
-      story: story || '',
-      year: year || '',
-      imageUrl: imageUrl || ''
-    };
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
-  validationSchema: Yup.object().shape({
-    author: Yup.string().required(),
-    country: Yup.string().required(),
-    title: Yup.string().required(),
-    story: Yup.string().required(),
-    year: Yup.number(),
-    imageUrl: Yup.string()
-  }),
-
-  handleSubmit(values, { props, setStatus, resetForm }) {
-    console.log(values);
-    axios
-      .post(
-        'https://refugee-stories-api-082019.herokuapp.com/api/public',
-        values
-      )
-      .then(res => {
-        console.log(res.data);
-        setStatus(res.data);
-        resetForm();
-      })
-      .catch(error => console.log(error.response));
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  story: {
+    height: 250,
+    width: 400,
+    labelOffset: -20
   }
-})(StoryForm);
+}));
 
-export default FormikStoryForm;
+function StoryForm(props) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <NavBar />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Share Your Story
+          </Typography>
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="name"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="title"
+                  label="Title of Story"
+                  name="title"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="imageurl"
+                  variant="outlined"
+                  fullWidth
+                  id="imageurl"
+                  label="Picture of Author"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="country"
+                  label="Author's Country of Origin"
+                  name="country"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  InputProps={{ classes: { input: classes.story } }}
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="story"
+                  label="Story"
+                  type="text"
+                  id="story"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default StoryForm;
+
+// https://refugee-stories-api-082019.herokuapp.com/api/public
--- src/components/landing/StoryRoute.js
+++ src/components/landing/StoryRoute.js
@@ -3,9 +3,10 @@ import { getStory } from '../../actions';
 import { connect } from 'react-redux';
 import NavBar from '../NavBar.js';
 import Footer from '../Footer.js';
+import Loader from 'react-loader-spinner';
 import { Typography } from '@material-ui/core';
 
-function StoryRoute({ story, getStory, match }) {
+function StoryRoute({ isLoading, story, getStory, match }) {
   useEffect(() => {
     getStory(match.params.id);
     console.log('CALLING THE API');
@@ -16,9 +17,15 @@ function StoryRoute({ story, getStory, match }) {
     <div>
       <NavBar />
       <div className="story-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#c96567" height={100} width={100} />
+        ) : (
+          ''
+        )}
         <Typography variant="h3" component="h3">
           {story.title}
         </Typography>
+
         <img className="story-picture" src={story.imageUrl} alt="author" />
         <Typography variant="h6" component="h6">
           by {story.author}
